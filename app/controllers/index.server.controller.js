292f733a37c1106ccfc64f93aa18df33db7cebfc
deteva <jchwjf@gmail.com>
2016-03-24 04:55:18 +0900

    set a ActivitySchema and create a new controller method that create the activity object by model

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -12,12 +12,21 @@ var config = require('../../config/config');
 var winston = require('winston');
 var async = require('async');
 var moment = require('moment');
-var nToday = moment().format('YYYY-MM-DD');
-winston.info(nToday);
-
 
 var fitbitApi = require('fitbit-node');
 var client = new fitbitApi(config.fitbit.clientID, config.fitbit.clientSecret );
+
+//get a average value from last week
+var averageLastWeek = function (weeksArray) {
+	var averageWeek = 0;
+	var totalWeek = 0;
+	weeksArray.forEach(function (eachDay) {
+		totalWeek += parseInt(eachDay["value"]);
+		return averageWeek = totalWeek / weeksArray.length;
+	});
+
+	return parseInt(averageWeek);
+};
 
 exports.connectFitbit = function(req, res, next) {
 	res.redirect(client.getAuthorizeUrl('activity heartrate nutrition sleep social', config.fitbit.callbackURL));
@@ -25,32 +34,77 @@ exports.connectFitbit = function(req, res, next) {
 
 exports.getFitbitData = function(req, res) {
 	client.getAccessToken(req.query.code, config.fitbit.callbackURL).then(function (result) {
-		var get1 = function(cb) {
-			client.get("/activities/steps/date/today/7d.json", result.access_token).then(function (res) {
+		var activity = {goals:{}, calories:{}, steps:{}, distance:{}, floors:{}, activityCalories:{}};
+		var nToday = moment().format('YYYY-MM-DD');
+		var baseDate = moment().subtract(7, 'days').format('YYYY-MM-DD');
+		winston.info('today' +
+			' is ' + nToday+ ' baseDate is ' + baseDate);
 
-				//res.send(results[0]);
-				console.log(res[0]);
-				cb(null, res[0]);
+		var getDailyActivity = function(callback) {
+			client.get('/activities/date/'+ nToday + '.json', result.access_token).then(function (res) {
+				activity.goals = res[0].goals;
+				winston.info(activity.goals);
+				callback();
 			});
 		};
 
-		var get2 = function(cb) {
-			client.get("/activities/date/today.json", result.access_token).then(function (res) {
-				//res.send(results[0]);
-				console.log(res[0]);
-				cb(null, res[0]);
+		var getCaloriesSeries = function(callback) {
+			client.get('/activities/calories/date/'+ nToday + '/' + baseDate + '.json', result.access_token).then(function (res) {
+				var responseObj = res[0]["activities-calories"];
+				var nLen = responseObj.length;
+				winston.info(responseObj);
+				activity.calories.weekAgoToday = parseInt(responseObj[0].value);
+				activity.calories.yesterday = parseInt(responseObj[nLen - 2].value);
+				activity.calories.today = parseInt(responseObj[nLen - 1].value);
+				activity.calories.lastWeek = averageLastWeek(responseObj);
+				winston.info(activity);
+				callback();
 			});
 		};
+
+//		var getStepsSeries = function(cb) {
+//			client.get('/activities/steps/date/'+ nToday + '/7d.json', result.access_token).then(function (res) {
+//				//res.send(results[0]);
+//				//console.log(res[0]);
+//				cb();
+//			});
+//		};
+//
+//		var getDistanceSeries = function(callback) {
+//			client.get('/activities/distance/date/'+ nToday + '/7d.json', result.access_token).then(function (res) {
+//				//winston.info(res[0]);
+//				callback();
+//			});
+//		};
+//
+//		var getFloorsSeries = function(callback) {
+//			client.get('/activities/floors/date/'+ nToday + '/7d.json', result.access_token).then(function (res) {
+//				//winston.info(res[0]);
+//				callback();
+//			});
+//		};
+//
+//		var getActivityCaloriesSeries = function(callback) {
+//			client.get('/activities/activityCalories/date/'+ nToday + '/7d.json', result.access_token).then(function (res) {
+//				//winston.info(res[0]);
+//				callback(null, res[0]);
+//			});
+//		};
 
 		async.series([
-			get1,
-			get2
+			getDailyActivity,
+			getCaloriesSeries
+//			getStepsSeries,
+//			getDistanceSeries,
+//			getFloorsSeries,
+//			getActivityCaloriesSeries
+
 		], function(err, results){
 			if(err) {
 				console.log('error'+ err);
 			}
-			console.log(typeof results);
-			res.send(results);
+			//console.log(typeof results);
+			res.send(activity);
 		})
 
 	}).catch(function (error) {
--- app/models/activity.server.model.js
+++ app/models/activity.server.model.js
@@ -3,13 +3,80 @@
  */
 // Load the module dependencies
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+	Schema = mongoose.Schema;
 
 // Define a new 'ActivitySchema'
 var ActivitySchema = new Schema({
-    name: String
+			goals: {
+				activeMinutes: {type: Number},
+				caloriesOut: {type: Number},
+				distance: {type: Number},
+				floors: {type: Number},
+				steps: {type: Number}
+			},
+			calories: {
+				today: {type: Number},
+				yesterday: {type: Number},
+				weekAgoToday: {type: Number},
+				lastWeek: {type: Number}
+			},
+			steps: {
+				today: {type: Number},
+				yesterday: {type: Number},
+				weekAgoToday: {type: Number},
+				lastWeek: {type: Number}
+			}
+			,
+			distance: {
+				today: {type: Number},
+				yesterday: {type: Number},
+				weekAgoToday: {type: Number},
+				lastWeek: {type: Number}
+			}
+			,
+			floors: {
+				today: {type: Number},
+				yesterday: {type: Number},
+				weekAgoToday: {type: Number},
+				lastWeek: {type: Number}
+			}
+			,
+			activityCalories: {
+				today: {type: Number},
+				yesterday: {type: Number},
+				weekAgoToday: {type: Number},
+				lastWeek: {type: Number}
+			}
+		}
+	);
+
+// Set the 'achievement' virtual property
+ActivitySchema.virtual('achievementActiveMinutes').get(function() {
+	return Math.floor((this.activeMinutes.today / this.goals.activeMinutes ) * 100);
+});
+
+ActivitySchema.virtual('achievementCaloriesOut').get(function() {
+	return Math.floor((this.calories.today / this.goals.caloriesOut ) * 100);
+});
+
+ActivitySchema.virtual('achievementDistance').get(function() {
+	return Math.floor((this.distance.today / this.goals.distance ) * 100);
+});
+
+ActivitySchema.virtual('achievementFloors').get(function() {
+	return Math.floor((this.floors.today / this.goals.floors ) * 100);
+});
+
+ActivitySchema.virtual('achievementSteps').get(function() {
+	return Math.floor((this.steps.today / this.goals.steps ) * 100);
+});
+
+
+ActivitySchema.set('toJSON', {
+	getters: true,
+	virtuals: true
 });
 
 // Create the 'Activity'
 // model out of the 'ActivitySchema'
- mongoose.model('Activity', ActivitySchema);
+mongoose.model('Activity', ActivitySchema);
